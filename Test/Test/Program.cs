d4d376e1823e822146a24347beb4f74a92a59eac
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-10-05 18:35:46 +0300

    Am adaugat numarul la pantof

---
+++ Test/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Scrie marimea de la pantof:");
+
+            string line;
+            line = Console.ReadLine();
+
+            int numarPantof;
+            numarPantof = int.Parse(line);
+
+            Console.WriteLine($"Aveti {numarPantof} numarul la pantof");
+        }
+    }
+}
